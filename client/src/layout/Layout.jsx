22b7fe090863d38697853b8b20e66f8e12b3d3ee
zahid-soft <zahidsarang95@gmail.com>
2023-08-28 20:31:05 +0530

    project dependencies installed

---
+++ client/src/layout/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import HomePage from "../pages/home/HomePage";
+import SideBar from "./SideBar";
+
+const Layout = () => {
+  return (
+    <div>
+     <SideBar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ client/src/pages/home/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// ============================= Component Import ================================== //
+import Home from "../../components/home/Home"
+
+const HomePage = () => {
+  return (
+    <div><Home /></div>
+  )
+}
+
+export default HomePage
